1594aef4ec459ab8e8741333358815e27e8a566c
Akhmedovdeveloper01 <developerakhmedov@gmail.com>
2026-03-06 16:31:35 +0500

    home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import AdminHome from "@/components/template/AdminHome/AdminHome";
+import StudentHome from "@/components/template/StudentHome/StudentHome";
+import SuperAdminHome from "@/components/template/SuperAdminHome/SuperAdminHome";
+import TeacherHome from "@/components/template/TeacherHome/TeacherHome";
+import { UserContext } from "@/context/UserContext";
+import React, { useContext } from "react";
+import NotFound from "../not-found/NotFound";
 
 export default function Home() {
-  return (
-    <div className="w-full">
-    </div>
-  );
+    const { roles } = useContext(UserContext);
+    switch (roles) {
+        case "ROLE_SYSTEM_ADMIN":
+            return <SuperAdminHome />;
+        case "ROLE_ADMIN":
+            return <AdminHome />;
+        case "ROLE_TEACHER":
+            return <TeacherHome />;
+        case "ROLE_STUDENT":
+            return <StudentHome />;
+        default:
+            return <NotFound />;
+    }
 }
